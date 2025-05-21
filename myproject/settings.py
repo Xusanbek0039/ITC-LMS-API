7a6eba398de82e731197d68e8bc1e7fe81ae8907
Xusanbek <109731657+Xusanbek0039@users.noreply.github.com>
2025-05-21 17:19:32 +0500

    Yangilandi

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,10 +11,20 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from .swagger_settings import SWAGGER_SETTINGS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Token': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    }
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
